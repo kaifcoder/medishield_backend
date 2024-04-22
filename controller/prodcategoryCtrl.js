8af3a8676df26c98f952903e9e49e49cb1ec5420
Mohd Kaif <kaifmohd2014@gmail.com>
2024-04-22 14:43:58 +0530

    Refactor sorting logic in getallBrand function and implement search functionality in getAllProductsAdmin function

--- controller/prodcategoryCtrl.js
+++ controller/prodcategoryCtrl.js
@@ -83,7 +83,11 @@ const getCategory = asyncHandler(async (req, res) => {
 
 const getallCategory = asyncHandler(async (req, res) => {
   try {
-    const getallCategory = await Category.find();
+    const getallCategory = await Category.find().
+      sort({
+        name: 1
+      })
+      ;
     res.json({
       "categories":
         getallCategory
