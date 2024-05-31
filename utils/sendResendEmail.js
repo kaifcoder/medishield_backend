db379c5337814131b398ee878805e604c3fd346d
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-31 19:30:36 +0530

    refactor: Update sendResendEmail function to use nodemailer instead of resend library

--- utils/sendResendEmail.js
+++ utils/sendResendEmail.js
@@ -1,7 +1,40 @@
-const resendImport = require("resend");
-const { Resend } = resendImport;
-require("dotenv").config();
-const resend = new Resend(process.env.RESEND_API);
+// const resendImport = require("resend");
+// const { Resend } = resendImport;
+// require("dotenv").config();
+// const resend = new Resend(process.env.RESEND_API);
+
+// async function sendResendEmail(
+//     to,
+//     subject,
+//     html,
+//     attachments = []
+// ) {
+//     const { data, error } = await resend.emails.send({
+//         from: 'MediShield-No-reply <medishield-NoReply@darkinc.tech>',
+//         to: [to],
+//         subject: subject,
+//         html: html,
+//         attachments: attachments
+//     });
+
+//     if (error) {
+//         return console.error({ error });
+//     }
+
+//     return data;
+// };
+// module.exports = { sendResendEmail }
+
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    }
+});
 
 async function sendResendEmail(
     to,
@@ -9,18 +42,24 @@ async function sendResendEmail(
     html,
     attachments = []
 ) {
-    const { data, error } = await resend.emails.send({
-        from: 'MediShield-No-reply <medishield-NoReply@darkinc.tech>',
-        to: [to],
+    const mailOptions = {
+        from: `${process.env.GMAIL_USER} <${process.env.GMAIL_USER}>`,
+        to: to,
         subject: subject,
         html: html,
-        attachments: attachments
-    });
+        attachments: attachments.map(file => ({
+            filename: file.filename,
+            path: file.path
+        }))
+    };
 
-    if (error) {
-        return console.error({ error });
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        return info;
+    } catch (error) {
+        console.error({ error });
+        return { error };
     }
-
-    return data;
 };
+
 module.exports = { sendResendEmail }
