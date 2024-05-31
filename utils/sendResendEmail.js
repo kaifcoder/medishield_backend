715e233b0ecc387756e0252a1ff20771c7c551d5
Mohd Kaif <kaifmohd2014@gmail.com>
2024-05-31 19:32:41 +0530

    refactor: Update GMAIL_PASS to GMAIL_APP_PASS in sendResendEmail.js

--- utils/sendResendEmail.js
+++ utils/sendResendEmail.js
@@ -32,7 +32,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
+        pass: process.env.GMAIL_APP_PASS
     }
 });
 
