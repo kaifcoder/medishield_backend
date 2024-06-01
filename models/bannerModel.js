e9b4121bc2232981298fe8f8db7fddffe449efff
Mohd Kaif <kaifmohd2014@gmail.com>
2024-06-01 12:19:58 +0530

    refactor: Add createdAt and updatedAt fields to bannerModel.js

--- models/bannerModel.js
+++ models/bannerModel.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const bannerSchema = new mongoose.Schema({
-
     mobile_image: String,
-
     title: String,
     id: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Banner = mongoose.model('Banner', bannerSchema);
